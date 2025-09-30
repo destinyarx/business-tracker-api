aee4a0034e32150c7bba1f476777a78dd103a06d
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2025-10-01 02:45:28 +0800

    send email function

---
+++ src/email/email.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
+
+@Injectable()
+export class EmailService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USER, // your Gmail
+        pass: process.env.GMAIL_PASS, // app password
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, text: string) {
+    const info = await this.transporter.sendMail({
+      from: `"My App" <${process.env.GMAIL_USER}>`,
+      to,
+      subject,
+      text,
+    });
+    console.log('Email sent: ', info.messageId);
+  }
+}
